0f5b7cd81afbf6b214d35f8254ff24e83b18c561
Mohammed Adel Mohammed <107713170+MoAdelEzz@users.noreply.github.com>
2024-04-13 19:14:35 +0200

    Implemented BigInteger Class

--- Application.cpp
+++ Application.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "BigInteger.h"
 using namespace std;
 
-void main()
+int main()
 {
+    BigInteger A("5651231861216165165");
+    BigInteger B("6313252351");
+
+    BigInteger C = A / B;
+
+    C.print();
+    return 0;
 }
